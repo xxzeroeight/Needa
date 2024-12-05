ccbdaedd8160086824123dd7be1ed722d8238a72
xxzeroeight <xxzeroeight@naver.com>
2024-12-05 11:35:48 +0900

    first commit

---
+++ src/main/java/com/market/needa/NeedaApplication.java
@@ -0,0 +1,13 @@
+package com.market.needa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/needa/NeedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.needa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
